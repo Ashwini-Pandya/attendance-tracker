ead5ff6fa7c8669227d08d93bc28ac78225dea7c
Ashwini-Pandya <ashwinipandya5@gmail.com>
2025-08-16 13:40:21 +0530

    Changes in Layout

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   { path: "/", element: <LandingPage /> },
-  { path: "/Absent-tracker", element: <App /> },
+  { path: "/App", element: <App /> },
   // { path: "*", element: <PageNotFound /> },
 ]);
 createRoot(document.getElementById("root")).render(
